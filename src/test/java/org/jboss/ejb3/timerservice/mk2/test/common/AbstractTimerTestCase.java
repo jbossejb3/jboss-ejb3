1d65ebae941d1e985ff5145a04e9c66e603344b8
Carlo de Wolf <cdewolf@redhat.com>
2009-03-03 10:14:32 +0000

    EJBTHREE-1749: Update to jboss-bootstrap 1.0.0-Beta-1

--- src/test/java/org/jboss/ejb3/timerservice/mk2/test/common/AbstractTimerTestCase.java
+++ src/test/java/org/jboss/ejb3/timerservice/mk2/test/common/AbstractTimerTestCase.java
@@ -149,6 +149,6 @@ public abstract class AbstractTimerTestCase
       boolean success = file.mkdirs() || file.isDirectory();
       if(!success)
          throw new IOException("failed to create " + path);
-      return file.toURI().toURL().toString();
+      return file.getAbsolutePath();
    }
 }
